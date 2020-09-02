ff3786fb41fe2e5a20f554dce8c8a6c4ec85fafd
caren1 <czarnocki.w@wp.pl>
2020-09-02 22:03:33 +0200

    Updates

---
+++ precache-manifest.f70ddd7cacde3309e55607d779b3027c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848c97306be255644a5f5be37b9a5e93",
+    "url": "/job-listings-project/index.html"
+  },
+  {
+    "revision": "6593999622f5818d183d",
+    "url": "/job-listings-project/static/css/main.364ba434.chunk.css"
+  },
+  {
+    "revision": "55338eea2736f08728b4",
+    "url": "/job-listings-project/static/js/2.b7134622.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-listings-project/static/js/2.b7134622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6593999622f5818d183d",
+    "url": "/job-listings-project/static/js/main.2b3ec2dd.chunk.js"
+  },
+  {
+    "revision": "a8cc54dd0ffef0359681",
+    "url": "/job-listings-project/static/js/runtime-main.c44060c0.js"
+  }
+]);
