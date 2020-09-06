d255811f8ae48084a01af18e13ea7a60fbe7bbf7
caren1 <czarnocki.w@wp.pl>
2020-09-06 11:14:47 +0200

    #readme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import data from './data.json'
+import data from './assets/data.json'
 import JobOffer from './components/JobOffer'
 
 function App() {
@@ -47,7 +47,7 @@ function App() {
 
     <div className="app">
       <header className="bg-teal-500 mb-12">
-        <img src="/images/bg-header-desktop.svg" alt="header-background" />
+        <img src="images/bg-header-desktop.svg" alt="header-background" />
       </header>
 
       {filters.length > 0 && (
